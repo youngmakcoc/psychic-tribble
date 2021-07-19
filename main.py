02ce81a57481fba78fc5b638e4b6672f113306b2
Михаил Тетенов <m.tetenov@trivium.group>
2021-07-20 00:03:38 +0500

    может работает, но иногда кидает 429 на первый запрос и, видимо, надо что то сделать со вторым

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print ("привет миша")
+import  requests
+
+phone = "9223702227"
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124",
+    "host": "api.sunlight.net", "Content-Type": "application/json", }
+
+def sunlihgt():
+    phoneSunlight = "7" + phone
+    s = requests.post('https://api.sunlight.net/v3/customers/authorization/', json={"phone":phoneSunlight}, headers=headers)
+    print(s.status_code)
+    print(s.json())
+
+def utair():
+    utPhone = phone + "+7"
+    u = requests.session()
+    u.get('https://b.utair.ru/api/v1/login/')
+    ut = requests.post('https://b.utair.ru/api/v1/login/', headers=headers, json={"login":utPhone})
+    print(ut.status_code)
+    print(ut.json())
+sunlihgt()
+utair()
